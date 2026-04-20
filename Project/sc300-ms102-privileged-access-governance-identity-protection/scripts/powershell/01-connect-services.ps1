29a48ec845bbf6537d1a3989f879174e139f4702
SeungHun Jeong <hm25200@gmail.com>
2026-04-20 15:41:00 -0400

    Update 01-connect-services.ps1

--- Project/sc300-ms102-privileged-access-governance-identity-protection/scripts/powershell/01-connect-services.ps1
+++ Project/sc300-ms102-privileged-access-governance-identity-protection/scripts/powershell/01-connect-services.ps1
@@ -1,1 +1,11 @@
+Import-Module Microsoft.Graph.Authentication -ErrorAction Stop
+Import-Module Microsoft.Graph.Users -ErrorAction Stop
+Import-Module Microsoft.Graph.Groups -ErrorAction Stop
+Import-Module Microsoft.Graph.Identity.SignIns -ErrorAction Stop
+Import-Module Microsoft.Graph.Identity.Governance -ErrorAction Stop
 
+Connect-MgGraph -Scopes "User.ReadWrite.All","Group.ReadWrite.All","Directory.ReadWrite.All","RoleManagement.ReadWrite.Directory","Policy.Read.All","Policy.ReadWrite.ConditionalAccess","Organization.Read.All"
+
+$context = Get-MgContext
+Write-Host "Connected Tenant ID: $($context.TenantId)"
+Write-Host "Connected Account : $($context.Account)"
