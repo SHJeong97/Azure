afd0f6f876c6d8f18f26bfcb18b0c736daa1bbfa
SeungHun Jeong <hm25200@gmail.com>
2026-04-21 16:56:29 -0400

    Update 01-create-ou-structure.ps1

--- Project/Hybrid-Identity-Foundation/scripts/powershell/01-create-ou-structure.ps1
+++ Project/Hybrid-Identity-Foundation/scripts/powershell/01-create-ou-structure.ps1
@@ -1,1 +1,19 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "corp.democompany1016.local" `
+-DomainNetbiosName "CORP" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
 
