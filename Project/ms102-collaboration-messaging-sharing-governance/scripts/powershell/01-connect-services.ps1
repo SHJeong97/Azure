fa968011218a1d1fa99bf8d465e7a7006f4c2099
SeungHun Jeong <hm25200@gmail.com>
2026-04-21 10:37:22 -0400

    Update 01-connect-services.ps1

--- Project/ms102-collaboration-messaging-sharing-governance/scripts/powershell/01-connect-services.ps1
+++ Project/ms102-collaboration-messaging-sharing-governance/scripts/powershell/01-connect-services.ps1
@@ -1,1 +1,9 @@
+Import-Module Microsoft.Graph.Authentication -ErrorAction Stop
+Import-Module Microsoft.Graph.Users -ErrorAction Stop
+Import-Module Microsoft.Graph.Groups -ErrorAction Stop
 
+Connect-MgGraph -Scopes "User.ReadWrite.All","Group.ReadWrite.All","Directory.ReadWrite.All","Organization.Read.All"
+
+$context = Get-MgContext
+Write-Host "Connected Tenant ID: $($context.TenantId)"
+Write-Host "Connected Account : $($context.Account)"
