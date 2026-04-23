c45ccff78ac3581c9f997655fd7503e4961271eb
SeungHun Jeong <hm25200@gmail.com>
2026-04-23 14:16:11 -0400

    Create 15-mail-flow-rule-notes.ps1

---
+++ Project/Hybrid Project/03-Exchange-Online-Mail-Flow-Security/scripts/powershell/15-mail-flow-rule-notes.ps1
@@ -0,0 +1,10 @@
+# Project 3 Batch 3 documentation notes
+# Rules created:
+# 1. MF01-Tag-External-Inbound-Mail
+# 2. MF02-Warn-External-Mail-To-Shared-Mailboxes
+# 3. MF03-Block-Inbound-Executable-Attachments
+#
+# Validation goals:
+# - external inbound message gets [External] in subject
+# - external message to shared mailbox gets disclaimer warning
+# - external message with executable attachment is blocked
